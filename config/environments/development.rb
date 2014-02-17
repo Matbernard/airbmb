f0ad0ad30e18f019b781c2e80f7edeb5d63d1734
Your Name <you@example.com>
2014-02-17 15:13:06 +0100

    first commit baby

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,7 @@ Airbmb::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  #set up the default url options for the Devise mailer in each environment.
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
 end
